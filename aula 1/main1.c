9a09d714c77ba099c99aeac28d99ecd94abe801c
Sofia Loren <127851693+sofialoren1@users.noreply.github.com>
2024-09-12 08:54:12 -0300

    Add files via upload

---
+++ aula 1/main1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Este é o primeiro exemplo do curso de lingaugem C
+
+int main(void) {
+  // Exibi as informações no console
+  printf("Meu primeiro trabalho em C");
+  printf("Meu nome é: Demétrio");
+  // Sucesso ao compilar o codigo
+  return 0;
+}
